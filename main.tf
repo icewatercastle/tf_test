74f79725312ce87f31e83bcb0696a2b630a8c870
Prateek Gangwal <pgangwal@vmware.com>
2021-11-02 23:25:54 -0700

    something

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
-resource "azurerm_resource_group" "example1" {
-  name     = "example-resources"
-  location = "West Europe"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
 }
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "marketing" {
+  name     = "marketing"
+  location = "northeurope"
+}
